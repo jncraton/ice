8c7c3a0f969cf05711b8287030d58cb6fcc148ca
Peyton3995 <143836277+Peyton3995@users.noreply.github.com>
2024-10-15 10:52:09 -0400

    Teacher content sharable (#16)
    * Implement rough sharable links
    
    * Remove hardcoded URL
    
    ---------
    
    Co-authored-by: jepoorbaugh <jepoorbaugh@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Load data from URL
+function loadDefaultData() {
+  if (location.hash !== '') {
+    const urlList = JSON.parse(atob(location.hash.split('#')[1]))
+
+    document.querySelector('#code-area').value = urlList[0]
+    document.querySelector('#output-text').value = urlList[1]
+  }
+}
+loadDefaultData()
+
+// Make share button generate link to current page.
+document.querySelector('#share').addEventListener('click', function () {
+  const codeText = document.querySelector('#code-area').value
+  const outputText = document.querySelector('#output-text').value
+
+  const base64String = btoa(JSON.stringify([codeText, outputText]))
+
+  const url = location.hostname + `#${base64String}`
+
+  document.querySelector('#link-display').innerHTML = url
+  document.querySelector('#link-display').href = url
+})
