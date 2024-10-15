6881409bef09c31d4ed711a572ddd30a3b4160dd
Owen Mockabee <omockabee@anderson.edu>
2024-10-15 09:37:42 -0400

    Fixes formatting errors

--- example.spec.js
+++ example.spec.js
@@ -1,13 +1,13 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('User can navigate and see expected text', async ({ page }) => {
-    // Step 1: Navigate to the page
-    await page.goto('https://jncraton.github.io/ice/');
+  // Step 1: Navigate to the page
+  await page.goto('https://jncraton.github.io/ice/')
 
-    // Step 2: Check the page title
-    await expect(page).toHaveTitle('I.C.E');
+  // Step 2: Check the page title
+  await expect(page).toHaveTitle('I.C.E')
 
-    // Step 3: Verify that the new page contains specific text
-    const expectedText = 'Hello World!';
-    await expect(page.locator('body')).toContainText(expectedText);
-});
+  // Step 3: Verify that the new page contains specific text
+  const expectedText = 'Hello World!'
+  await expect(page.locator('body')).toContainText(expectedText)
+})
