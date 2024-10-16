3c787a1c737f3cfaf3520037ff5a06f91fdac128
Owen Mockabee <omockabee@anderson.edu>
2024-10-16 10:36:30 -0400

    Look at index.html file locally rather than a URL

---
+++ testSpec.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Page, expect
+import os
+
+current_working_directory = os.getcwd()
+file_name = "index.html"
+full_path = os.path.join(current_working_directory, file_name)
+
+def test_title(page: Page):
+    #  Step 1: Navigate to the index.html page
+    page.goto(full_path)
+
+    #  Step 2: Check the page title
+    expect(page).to_have_title('I.C.E')
+
+def test_text(page: Page):
+    #  Step 1: Navigate to the index.html page
+    page.goto(full_path)
+    
+    #  Step 2: Verify that the new page contains specific text
+    expectedText = 'Hello World!'
+    expect(page.locator('body')).to_contain_text(expectedText)
