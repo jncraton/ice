9060129b44745dea637a2f7e4240c01c3dab56f4
AndrewG <amgeiger@anderson.edu>
2024-10-21 10:49:09 -0400

    remove hello world from the page and remove the test for it

--- testSpec.py
+++ testSpec.py
@@ -11,11 +11,3 @@ def test_title(page: Page):
 
     #  Step 2: Check the page title
     expect(page).to_have_title('Integrative Coding Experience')
-
-def test_text(page: Page):
-    #  Step 1: Navigate to the index.html page
-    page.goto(full_path)
-    
-    #  Step 2: Verify that the new page contains specific text
-    expectedText = 'Hello World!'
-    expect(page.locator('body')).to_contain_text(expectedText)
