4fd17ef60923a7fe204f51a1190e2f98fba39699
AndrewG2026 <144158464+AndrewG2026@users.noreply.github.com>
2024-10-22 10:32:30 -0400

    add testing for output text (#44)
    Co-authored-by: AndrewG <amgeiger@anderson.edu>

--- testSpec.py
+++ testSpec.py
@@ -11,3 +11,11 @@ def test_title(page: Page):
 
     #  Step 2: Check the page title
     expect(page).to_have_title('Integrative Coding Experience')
+
+def test_desired_outcome(page: Page):
+    #  Step 1: Navigate to the index.html page
+    page.goto(full_path)
+    
+    # Step 2: Check the page for a spot for desired output
+    textbox_locator = page.locator("#output-text")
+    assert textbox_locator.is_visible()
