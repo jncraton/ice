d063c124f01b6adc33ad072d96f2aa23d611ebe0
Peyton <peyton3995@outlook.com>
2024-10-23 10:24:32 -0400

    Pushes up buggy UI test

--- mainPageTest.py
+++ mainPageTest.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 import os
+import time
 
 current_working_directory = os.getcwd()
 file_name = "index.html"
@@ -19,3 +20,30 @@ def test_desired_outcome(page: Page):
     # Step 2: Check the page for a spot for desired output
     textbox_locator = page.locator("#output-text")
     assert textbox_locator.is_visible()
+
+def test_student_link_navigates(page: Page):
+
+    # This functionality can only truly be tested on a live webpage
+    # GitHub pages presents the best environment to do this in.
+    # Step 1: Navigate to index.html on github page
+    page.goto(full_path)
+
+    # Step 2: Input text in input field
+    page.locator("#code-area").fill("test input")
+
+    # Step 3: Input text in output field
+    page.locator("#output-text").fill("test output")
+
+    # Step 4: Pushing the (share) Button
+    page.locator("#share").click()
+
+    # Step 5: Checking for Alert 
+    page.locator("#link-display").click() 
+
+    time.sleep(5)
+
+    expect(page.locator("#alert")).to_be_visible()
+    expect(page.locator("#alert")).to_have_text("Link copied to clipboard")
+
+    
+    
