c33389144fa9c7f0e8b1a1f3dfb1e44d0d58e58d
jepoorbaugh <jepoorbaugh@gmail.com>
2024-10-25 09:47:17 -0400

    Fix formatting

--- src/student.js
+++ src/student.js
@@ -2,7 +2,7 @@
 function loadDefaultData() {
   if (location.hash !== '') {
     const urlList = JSON.parse(atob(location.hash.split('#')[1]))
-    
+
     document.querySelector('#code-area').value = urlList[0]
     document.querySelector('#output-text').value = urlList[1]
   }
@@ -11,12 +11,12 @@ loadDefaultData()
 
 // Create and configure a new web worker to run python code
 function createCodeWorker() {
-  const codeWorker = new Worker("/src/worker.js")
+  const codeWorker = new Worker('/src/worker.js')
 
-  codeWorker.addEventListener("message", function (msg) {
-    console.log("Message received")
-  
-    if (msg.data.type === "result") {
+  codeWorker.addEventListener('message', function (msg) {
+    console.log('Message received')
+
+    if (msg.data.type === 'result') {
       document.querySelector('#code-output').innerHTML = msg.data.result
       runButton.disabled = false
       endButton.disabled = true
@@ -31,29 +31,29 @@ let codeWorker = createCodeWorker()
 // let workerIsDead = false
 
 // get HTML elements
-const runButton = document.querySelector("#run-button")
-const endButton = document.querySelector("#end-button")
-const timeDisplayP = document.querySelector("#time-displayed")
+const runButton = document.querySelector('#run-button')
+const endButton = document.querySelector('#end-button')
+const timeDisplayP = document.querySelector('#time-displayed')
 
-// Run code when button pressed. 
-runButton.addEventListener("click", function () {
-  // 
+// Run code when button pressed.
+runButton.addEventListener('click', function () {
+  //
   // if (workerIsDead) {
   //   codeWorker = createCodeWorker()
   // }
-  const studentCode = document.querySelector("#code-area").value
+  const studentCode = document.querySelector('#code-area').value
   runButton.disabled = true
   endButton.disabled = false
 
-  console.log("Posting message")
+  console.log('Posting message')
   codeWorker.postMessage({
-    type: "run",
-    language: "python",
+    type: 'run',
+    language: 'python',
     code: studentCode,
   })
 })
 
-endButton.addEventListener("click", function () {
+endButton.addEventListener('click', function () {
   codeWorker.terminate()
   codeWorker = createCodeWorker() // send in the next worker
 
--- src/worker.js
+++ src/worker.js
@@ -1,37 +1,39 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.26.3/full/pyodide.js");
+importScripts('https://cdn.jsdelivr.net/pyodide/v0.26.3/full/pyodide.js')
 
 async function configurePyodide() {
-    self.pyodide = await loadPyodide()
-    self.pythonConsoleString = ""
-    await self.pyodide.setStdout({ batched: function (msg) { self.pythonConsoleString += `\n${msg}` } })
+  self.pyodide = await loadPyodide()
+  self.pythonConsoleString = ''
+  await self.pyodide.setStdout({
+    batched: function (msg) {
+      self.pythonConsoleString += `\n${msg}`
+    },
+  })
 }
 
 let configurePyodidePromise = configurePyodide()
 
-
 // Run python code and print the output
 async function runPythonCode(code) {
-    console.log("Running code")
-    
-    await configurePyodidePromise
-    self.pythonConsoleString = ""
-    // let pythonConsoleString = ""
-    // pyodide.setStdout({ batched: function (msg) { pythonConsoleString += `\n${msg}` } })
+  console.log('Running code')
 
-    let result = await pyodide.runPythonAsync(code)
+  await configurePyodidePromise
+  self.pythonConsoleString = ''
+  // let pythonConsoleString = ""
+  // pyodide.setStdout({ batched: function (msg) { pythonConsoleString += `\n${msg}` } })
 
-    return self.pythonConsoleString + `\n\n${result}`
+  let result = await pyodide.runPythonAsync(code)
+
+  return self.pythonConsoleString + `\n\n${result}`
 }
 
-this.addEventListener("message", function (msg) {
-    console.log("Encountered message")
-    if (msg.data.type === "run" && msg.data.language === "python") {
-        
-        runPythonCode(msg.data.code).then(function (result) {
-            this.postMessage({
-                type: "result",
-                result: result,
-            })
-        })
-    }
+this.addEventListener('message', function (msg) {
+  console.log('Encountered message')
+  if (msg.data.type === 'run' && msg.data.language === 'python') {
+    runPythonCode(msg.data.code).then(function (result) {
+      this.postMessage({
+        type: 'result',
+        result: result,
+      })
+    })
+  }
 })
