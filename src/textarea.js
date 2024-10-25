6fab2b4182ae332474dfc737fffc2ce18097ffbb
jepoorbaugh <jepoorbaugh@gmail.com>
2024-10-25 09:23:02 -0400

    Add comments to `textarea.js`

--- src/textarea.js
+++ src/textarea.js
@@ -1,3 +1,5 @@
+// Allow user to type tabs in code area
+// This is in its own file so code is not duplicated across index.js and student.js
 codeArea = document.querySelector("#code-area")
 codeArea.addEventListener("keydown", function (event) {
     if (event.key === "Tab") {
