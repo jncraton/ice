02b27c127196443d9b8658622b6a3ade604d91e3
Owen Mockabee <omockabee@anderson.edu>
2024-10-28 10:21:32 -0400

    Fix linter errors

--- www/student.js
+++ www/student.js
@@ -25,55 +25,55 @@ document.querySelector('#run').addEventListener('click', function () {
     label.classList.add('labelIncorrect')
     label.classList.remove('labelCorrect')
   }
-// Create and configure a new web worker to run python code
-function createCodeWorker() {
-  const codeWorker = new Worker('/worker.js')
+  // Create and configure a new web worker to run python code
+  function createCodeWorker() {
+    const codeWorker = new Worker('/worker.js')
 
-  codeWorker.addEventListener('message', function (msg) {
-    console.log('Message received')
+    codeWorker.addEventListener('message', function (msg) {
+      console.log('Message received')
 
-    if (msg.data.type === 'result') {
-      document.querySelector('#code-output').innerHTML = msg.data.result
-      runButton.disabled = false
-      endButton.disabled = true
-    }
+      if (msg.data.type === 'result') {
+        document.querySelector('#code-output').innerHTML = msg.data.result
+        runButton.disabled = false
+        endButton.disabled = true
+      }
+    })
+
+    return codeWorker
+  }
+
+  // Run python code in web worker and deal with run button
+  let codeWorker = createCodeWorker()
+  // let workerIsDead = false
+
+  // get HTML elements
+  const runButton = document.querySelector('#run-button')
+  const endButton = document.querySelector('#end-button')
+  const timeDisplayP = document.querySelector('#time-displayed')
+
+  // Run code when button pressed.
+  runButton.addEventListener('click', function () {
+    //
+    // if (workerIsDead) {
+    //   codeWorker = createCodeWorker()
+    // }
+    const studentCode = document.querySelector('#code-area').value
+    runButton.disabled = true
+    endButton.disabled = false
+
+    console.log('Posting message')
+    codeWorker.postMessage({
+      type: 'run',
+      language: 'python',
+      code: studentCode,
+    })
   })
 
-  return codeWorker
-}
+  endButton.addEventListener('click', function () {
+    codeWorker.terminate()
+    codeWorker = createCodeWorker() // send in the next worker
 
-// Run python code in web worker and deal with run button
-let codeWorker = createCodeWorker()
-// let workerIsDead = false
-
-// get HTML elements
-const runButton = document.querySelector('#run-button')
-const endButton = document.querySelector('#end-button')
-const timeDisplayP = document.querySelector('#time-displayed')
-
-// Run code when button pressed.
-runButton.addEventListener('click', function () {
-  //
-  // if (workerIsDead) {
-  //   codeWorker = createCodeWorker()
-  // }
-  const studentCode = document.querySelector('#code-area').value
-  runButton.disabled = true
-  endButton.disabled = false
-
-  console.log('Posting message')
-  codeWorker.postMessage({
-    type: 'run',
-    language: 'python',
-    code: studentCode,
+    runButton.disabled = false
+    endButton.disabled = true
   })
-})
-
-endButton.addEventListener('click', function () {
-  codeWorker.terminate()
-  codeWorker = createCodeWorker() // send in the next worker
-
-  runButton.disabled = false
-  endButton.disabled = true
-})
 })
