6925cefad92312ba7df5451d27e74b8de22e91e2
jepoorbaugh <34549460+jepoorbaugh@users.noreply.github.com>
2024-10-28 10:07:33 -0400

    Run student python in broswer (#56)
    * Add HTML for python running
    
    * Add basic ability to run python code
    
    * Allow user to type tabs in text area
    
    * Change code area tabs to tab characters to change spacing with CSS
    
    * Move python execution to web worker
    
    * Add comments to `textarea.js`
    
    * Add end button to instantly terminate code
    
    * Fix formatting
    
    * Fix formatting
    
    * Change output to never display undefined.
    
    * Fix formatting

---
+++ www/student.js
@@ -0,0 +1,62 @@
+// Load data from URL
+function loadDefaultData() {
+  if (location.hash !== '') {
+    const urlList = JSON.parse(atob(location.hash.split('#')[1]))
+
+    document.querySelector('#code-area').value = urlList[0]
+    document.querySelector('#output-text').value = urlList[1]
+  }
+}
+loadDefaultData()
+
+// Create and configure a new web worker to run python code
+function createCodeWorker() {
+  const codeWorker = new Worker('/worker.js')
+
+  codeWorker.addEventListener('message', function (msg) {
+    console.log('Message received')
+
+    if (msg.data.type === 'result') {
+      document.querySelector('#code-output').innerHTML = msg.data.result
+      runButton.disabled = false
+      endButton.disabled = true
+    }
+  })
+
+  return codeWorker
+}
+
+// Run python code in web worker and deal with run button
+let codeWorker = createCodeWorker()
+// let workerIsDead = false
+
+// get HTML elements
+const runButton = document.querySelector('#run-button')
+const endButton = document.querySelector('#end-button')
+const timeDisplayP = document.querySelector('#time-displayed')
+
+// Run code when button pressed.
+runButton.addEventListener('click', function () {
+  //
+  // if (workerIsDead) {
+  //   codeWorker = createCodeWorker()
+  // }
+  const studentCode = document.querySelector('#code-area').value
+  runButton.disabled = true
+  endButton.disabled = false
+
+  console.log('Posting message')
+  codeWorker.postMessage({
+    type: 'run',
+    language: 'python',
+    code: studentCode,
+  })
+})
+
+endButton.addEventListener('click', function () {
+  codeWorker.terminate()
+  codeWorker = createCodeWorker() // send in the next worker
+
+  runButton.disabled = false
+  endButton.disabled = true
+})
