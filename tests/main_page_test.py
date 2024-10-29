2ff79af8bf4ce2c22e7c46c984fd68fba11978f0
Jon Craton <jncraton@gmail.com>
2024-10-29 08:56:40 -0400

    Refactor main page tests

--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -1,44 +1,46 @@
-from playwright.sync_api import Page, expect
-import os
+"""
+Tests for the teacher view of the application
 
-current_working_directory = os.getcwd()
-full_path = "http://localhost:8000"
+These are the end-to-end UI test for index.html
+"""
+
+from playwright.sync_api import Page, expect
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each(page: Page):
+    """Load the page before each test"""
+    page.goto("http://localhost:8000")
 
 
 def test_title(page: Page):
-    #  Step 1: Navigate to the index.html page
-    page.goto(full_path)
+    """Confirm the page has an appropriate title"""
 
-    #  Step 2: Check the page title
     expect(page).to_have_title("Integrative Coding Experience")
 
 
 def test_desired_outcome(page: Page):
-    #  Step 1: Navigate to the index.html page
-    page.goto(full_path)
+    """Confirm that output text is visible"""
 
-    # Step 2: Check the page for a spot for desired output
     textbox_locator = page.locator("#output-text")
     assert textbox_locator.is_visible()
 
 
 def test_student_link_navigates(page: Page):
-    # This functionality can only truly be tested on a live webpage
-    # GitHub pages presents the best environment to do this in.
-    # Step 1: Navigate to index.html on github page
-    page.goto(full_path)
+    """Confirm that student link copies correctly"""
 
-    # Step 2: Input text in input field
+    # Step 1: Input text in input field
     page.locator("#code-area").fill("test input")
 
-    # Step 3: Input text in output field
+    # Step 2: Input text in output field
     page.locator("#output-text").fill("test output")
 
-    # Step 4: Pushing the (share) Button
+    # Step 3: Pushing the (share) Button
     page.context.grant_permissions(["clipboard-write"])
     page.locator("#share").click()
 
-    # Step 5: Checking for Alert
+    # Step 4: Checking for Alert
     page.locator("#link-display").click()
 
     expect(page.locator("#alert")).to_be_visible()
