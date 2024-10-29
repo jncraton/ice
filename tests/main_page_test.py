84e9e00fca617661e1204e8c108cb1897e2c1de4
Jon Craton <jncraton@gmail.com>
2024-10-29 08:43:00 -0400

    Reformat with black

--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -4,23 +4,25 @@ import os
 current_working_directory = os.getcwd()
 full_path = "http://localhost:8000"
 
+
 def test_title(page: Page):
     #  Step 1: Navigate to the index.html page
     page.goto(full_path)
 
     #  Step 2: Check the page title
-    expect(page).to_have_title('Integrative Coding Experience')
+    expect(page).to_have_title("Integrative Coding Experience")
+
 
 def test_desired_outcome(page: Page):
     #  Step 1: Navigate to the index.html page
     page.goto(full_path)
-    
+
     # Step 2: Check the page for a spot for desired output
     textbox_locator = page.locator("#output-text")
     assert textbox_locator.is_visible()
 
-def test_student_link_navigates(page: Page):
 
+def test_student_link_navigates(page: Page):
     # This functionality can only truly be tested on a live webpage
     # GitHub pages presents the best environment to do this in.
     # Step 1: Navigate to index.html on github page
@@ -36,7 +38,7 @@ def test_student_link_navigates(page: Page):
     page.context.grant_permissions(["clipboard-write"])
     page.locator("#share").click()
 
-    # Step 5: Checking for Alert 
+    # Step 5: Checking for Alert
     page.locator("#link-display").click()
 
     expect(page.locator("#alert")).to_be_visible()
--- tests/student_page_test.py
+++ tests/student_page_test.py
@@ -5,12 +5,14 @@ current_working_directory = os.getcwd()
 file_name = "www/student.html"
 full_path = "file://" + os.path.join(current_working_directory, file_name)
 
+
 def test_title(page: Page):
     #  Step 1: Navigate to the student.html page
     page.goto(full_path)
 
     #  Step 2: Check the page title
-    expect(page).to_have_title('Integrative Coding Experience')
+    expect(page).to_have_title("Integrative Coding Experience")
+
 
 def test_student_input(page: Page):
     #  Step 1: Navigate to the student.html page
