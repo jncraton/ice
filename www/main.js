2e1ae815738e37dbde6cfde77fcb367597d31ed9
taigercub <tbvandam@anderson.edu>
2024-10-30 10:42:43 -0400

    Run python code on instructor view and save output to expected output

--- www/main.js
+++ www/main.js
@@ -11,6 +11,63 @@ function loadDefaultData() {
 }
 loadDefaultData()
 
+// Create and configure a new web worker to run python code
+function createCodeWorker() {
+  const codeWorker = new Worker('/worker.js')
+
+  codeWorker.addEventListener('message', function (msg) {
+    console.log('Message received')
+
+    if (msg.data.type === 'result') {
+      document.querySelector('#code-output').innerHTML = msg.data.result
+      runButton.disabled = false
+      endButton.disabled = true
+    }
+  })
+
+  return codeWorker
+}
+
+// Run python code in web worker and deal with run button
+let codeWorker = createCodeWorker()
+// let workerIsDead = false
+
+// get HTML elements
+const runButton = document.querySelector('#run-button')
+const endButton = document.querySelector('#end-button')
+const timeDisplayP = document.querySelector('#time-displayed')
+const saveOutput = document.querySelector('#save-output')
+
+// Run code when button pressed.
+runButton.addEventListener('click', function () {
+  //
+  // if (workerIsDead) {
+  //   codeWorker = createCodeWorker()
+  // }
+  const studentCode = document.querySelector('#code-area').value
+  runButton.disabled = true
+  endButton.disabled = false
+
+  console.log('Posting message')
+  codeWorker.postMessage({
+    type: 'run',
+    language: 'python',
+    code: studentCode,
+  })
+})
+
+endButton.addEventListener('click', function () {
+  codeWorker.terminate()
+  codeWorker = createCodeWorker() // send in the next worker
+
+  runButton.disabled = false
+  endButton.disabled = true
+})
+
+saveOutput.addEventListener('click', function() {
+  document.querySelector('#output-text').value = document.querySelector('#code-output').innerHTML
+})
+
 // Make share button generate link to current page.
 document.querySelector('#share').addEventListener('click', function () {
   const codeText = document.querySelector('#code-area').value
@@ -45,18 +102,3 @@ linkDisplay.addEventListener('click', function (event) {
   )
 })
 
-// Make check output button function
-document.querySelector('#run').addEventListener('click', function () {
-  const codeText = document.querySelector('#code-area').value
-  const outputText = document.querySelector('#output-text').value
-  const label = document.querySelector('#check-code-result')
-  if (codeText === outputText) {
-    label.textContent = 'Correct   ✔'
-    label.classList.add('labelCorrect')
-    label.classList.remove('labelIncorrect')
-  } else {
-    label.textContent = 'Does not match target output   ❌'
-    label.classList.add('labelIncorrect')
-    label.classList.remove('labelCorrect')
-  }
-})
