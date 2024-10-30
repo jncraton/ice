46fd7f9991be03f1de8dd8720d9952eb57ca10b9
Owen Mockabee <omockabee@anderson.edu>
2024-10-30 10:54:10 -0400

    Merge in main

--- tests/student_page_test.py
+++ tests/student_page_test.py
@@ -1,21 +1,68 @@
-from playwright.sync_api import Page, expect
-import os
+"""
+Tests for the student view of the application
 
-current_working_directory = os.getcwd()
-file_name = "www/student.html"
-full_path = "file://" + os.path.join(current_working_directory, file_name)
+These are the end-to-end UI tests for student.html
+"""
+
+from playwright.sync_api import Page, expect
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each(page: Page):
+    """Load the page before each test"""
+    page.goto("http://localhost:8000/student.html")
+
 
 def test_title(page: Page):
-    #  Step 1: Navigate to the student.html page
-    page.goto(full_path)
+    """Confirm the page has an appropriate title"""
 
-    #  Step 2: Check the page title
-    expect(page).to_have_title('Integrative Coding Experience')
+    expect(page).to_have_title("Integrative Coding Experience")
+
 
 def test_student_input(page: Page):
-    #  Step 1: Navigate to the student.html page
-    page.goto(full_path)
+    """Confirm that output text is visible"""
 
-    # Step 2: Test that text area is on page
     textarea_locator = page.locator("#output-text")
     assert textarea_locator.is_visible()
+
+
+def test_python_runs(page: Page):
+    """
+    Test that basic python code can execute
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill('print("Hello, world!")')
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Assert desired output is
+    expect(page.locator("#code-output")).to_have_text("Hello, world!")
+
+
+def test_buttons_disable(page: Page):
+    """
+    Test that running a python program enables/disables the right buttons,
+    and stopping it from running enables/disables the correct buttons.
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill("while True:\n\tprint(1)")
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Expect run button to be disabled, end button to be enabled
+    expect(page.locator("#run-button")).to_be_disabled()
+    expect(page.locator("#end-button")).to_be_enabled()
+
+    # 4. Click the End button
+    page.locator("#end-button").click()
+
+    # 5. Expect the run button to be enabled, end button to be disabled.
+    expect(page.locator("#run-button")).to_be_enabled()
+    expect(page.locator("#end-button")).to_be_disabled()
