1a1013431ed60f73cf7c401b63d1c782430508b7
jepoorbaugh <34549460+jepoorbaugh@users.noreply.github.com>
2024-10-30 10:16:19 -0400

    60 student python tests (#63)
    * Add HTML for python running
    
    * Add basic ability to run python code
    
    * Allow user to type tabs in text area
    
    * Change code area tabs to tab characters to change spacing with CSS
    
    * Move python execution to web worker
    
    * Add comments to `textarea.js`
    
    * Add end button to instantly terminate code
    
    * Fix formatting
    
    * Fix formatting
    
    * Change output to never display undefined.
    
    * Fix formatting
    
    * Fix bug where "end" button is not disabled after page refresh
    
    * Add test for running python code
    
    * Add test to ensure infinite loops can be terminated
    
    * Fix bug where buttons are not re-enabled
    
    * Fixed formatting
    
    * Fix formatting
    
    * Fix issue where boolean html value is set to true
    
    * Fix formatting of tests
    
    * Revert changes made to `student.js`

--- tests/student_page_test.py
+++ tests/student_page_test.py
@@ -25,3 +25,44 @@ def test_student_input(page: Page):
 
     textarea_locator = page.locator("#output-text")
     assert textarea_locator.is_visible()
+
+
+def test_python_runs(page: Page):
+    """
+    Test that basic python code can execute
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill('print("Hello, world!")')
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Assert desired output is
+    expect(page.locator("#code-output")).to_have_text("Hello, world!")
+
+
+def test_buttons_disable(page: Page):
+    """
+    Test that running a python program enables/disables the right buttons,
+    and stopping it from running enables/disables the correct buttons.
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill("while True:\n\tprint(1)")
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Expect run button to be disabled, end button to be enabled
+    expect(page.locator("#run-button")).to_be_disabled()
+    expect(page.locator("#end-button")).to_be_enabled()
+
+    # 4. Click the End button
+    page.locator("#end-button").click()
+
+    # 5. Expect the run button to be enabled, end button to be disabled.
+    expect(page.locator("#run-button")).to_be_enabled()
+    expect(page.locator("#end-button")).to_be_disabled()
