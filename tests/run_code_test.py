2eded1b472769e3df107a71dbf4731591d2772b6
taigercub <tbvandam@anderson.edu>
2024-10-30 21:18:13 -0400

    combining code run tests (currently unworking)

---
+++ tests/run_code_test.py
@@ -0,0 +1,52 @@
+"""
+Tests for the ability to run code in both the student and teacher view of the application
+"""
+
+from playwrite.sync_api import Page, expect
+import pytest
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each(page: Page):
+    """Load the page before each test"""
+    page.goto("http://localhost:8000")
+
+
+def test_python_runs(page: Page):
+    """
+    Test that basic python code can execute
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill('print("Hello, world!")')
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Assert desired output is
+    expect(page.locator("#code-output")).to_have_text("Hello, world!")
+
+
+def test_buttons_disable(page: Page):
+    """
+    Test that running a python program enables/disables the right buttons,
+    and stopping it from running enables/disables the correct buttons.
+    """
+
+    # 1. Put code in code area
+    textarea_locator = page.locator("#code-area")
+    textarea_locator.fill("while True:\n\tprint(1)")
+
+    # 2. Click Run Button
+    page.locator("#run-button").click()
+
+    # 3. Expect run button to be disabled, end button to be enabled
+    expect(page.locator("#run-button")).to_be_disabled()
+    expect(page.locator("#end-button")).to_be_enabled()
+
+    # 4. Click the End button
+    page.locator("#end-button").click()
+
+    # 5. Expect the run button to be enabled, end button to be disabled.
+    expect(page.locator("#run-button")).to_be_enabled()
+    expect(page.locator("#end-button")).to_be_disabled()
