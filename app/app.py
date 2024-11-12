938587fa88f0548765cf74b6fa06985fc9404701
jepoorbaugh <jepoorbaugh@gmail.com>
2024-11-12 11:00:44 -0500

    Start adding API endpoints for database

--- app/app.py
+++ app/app.py
@@ -7,10 +7,12 @@
 import sqlite3
 import os.path
 from flask import Flask, request, g
+from datetime import datetime
+import time
 
 app = Flask(__name__, static_folder="../www")
 
-DATABASE_PATH = "db/ice-database.db"
+DATABASE_PATH = "../app/db/ice-database.db"
 
 
 # Set up database
@@ -21,7 +23,7 @@ def get_db():
     """
     db = getattr(g, "_database", None)
     if db is None:
-        if os.path().isfile(DATABASE_PATH):
+        if os.path.isfile(DATABASE_PATH):
             # If the database file exists, load it. Otherwise, init the database.
             db = g._database = sqlite3.connect(DATABASE_PATH)
         else:
@@ -39,6 +41,7 @@ def close_db(exception):
     """Closes the connection to the database when the app context is destroyed."""
     db = getattr(g, "_database", None)
     if db is not None:
+        db.commit()
         db.close()
 
 
@@ -65,3 +68,55 @@ def serve_site(filename):
     """Correctly route all elements within the `www` directory"""
     # pylint: disable=unused-argument
     return app.send_static_file(request.path.lstrip("/"))
+
+
+# API routing
+@app.route("/api/student}")
+def add_student():
+    # return query_db("INSERT INTO student ()")
+    pass
+
+
+@app.route("/api/activity}")
+def add_activity():
+    pass
+
+
+@app.route("/api/section", methods=["GET", "POST"])
+def add_section():
+    if request.method == "POST":
+        try:
+            query_db(
+                "INSERT INTO section (txt_section_name, txt_instructor_name, ts_time_recorded) VALUES (?, ?, ?);",
+                (
+                    request.json["section_name"],
+                    request.json["instructor_name"],
+                    time.mktime(datetime.now().timetuple()),
+                ),
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
+
+        return list(dict(row) for row in query_db("SELECT * FROM section;"))
+
+    elif request.method == "GET":
+        try:
+            return list(
+                dict(row)
+                for row in query_db(
+                    "SELECT * FROM section WHERE txt_section_name = ? AND txt_instructor_name = ? AND ts_time_recorded BETWEEN ? AND ?;",
+                    (
+                        request.json["section_name"],
+                        request.json["instructor_name"],
+                        request.json["timestamp_min"],
+                        request.json["timestamp_max"],
+                    ),
+                )
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
+
+
+@app.route("/api/submission}", methods=["GET", "POST"])
+def add_submission():
+    pass
