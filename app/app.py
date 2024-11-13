f537fcce3be0c2050a9e570ab33d9de75b9d628b
jepoorbaugh <jepoorbaugh@gmail.com>
2024-11-13 10:54:53 -0500

    Fix bug where schema is not read properly

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ def get_db():
         else:
             db = g._database = sqlite3.connect(DATABASE_PATH)
             with app.open_resource("db/schema.sql") as f:
-                db.cursor().executescript(f.read())
+                db.cursor().executescript(f.read().decode("utf-8"))
             db.commit()
 
     db.row_factory = sqlite3.Row
