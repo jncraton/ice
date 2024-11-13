06225a68d57fd99cd88b4f7863da62cdbf805e7d
jepoorbaugh <jepoorbaugh@gmail.com>
2024-11-13 10:46:54 -0500

    Implement API endpoints

--- app/app.py
+++ app/app.py
@@ -72,18 +72,91 @@ def serve_site(filename):
 
 # API routing
 @app.route("/api/student}")
-def add_student():
-    # return query_db("INSERT INTO student ()")
-    pass
+def api_student():
+    """Handle GET and POST requests made to /api/student.
+    On a GET request, returns an array of dictionaries which represent the results of the query made
+    On a POST request, adds the designated exercise to the database and returns the entire exercise table to show that the record has been added.
+    """
+    if request.method == "POST":
+        try:
+            query_db(
+                "INSERT INTO student (txt_student_name, fk_section_id, ts_time_recorded) VALUES (?, ?, ?);",
+                (
+                    request.json["student_name"],
+                    request.json["section_id"],
+                    time.mktime(datetime.now().timetuple()),
+                ),
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
+
+        return list(dict(row) for row in query_db("SELECT * FROM student;"))
+
+    elif request.method == "GET":
+        try:
+            return list(
+                dict(row)
+                for row in query_db(
+                    "SELECT * FROM student WHERE fk_section_id = ? AND txt_student_name LIKE ? AND ts_time_recorded BETWEEN ? AND ?;",
+                    (
+                        request.json["section_id"],
+                        f'%{request.json["student_name"]}%',
+                        request.json["timestamp_min"],
+                        request.json["timestamp_max"],
+                    ),
+                )
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
 
 
-@app.route("/api/activity}")
-def add_activity():
-    pass
+@app.route("/api/exercise}")
+def api_exercise():
+    """Handle GET and POST requests made to /api/exercise.
+    On a GET request, returns an array of dictionaries which represent the results of the query made
+    On a POST request, adds the designated exercise to the database and returns the entire exercise table to show that the record has been added.
+    """
+    if request.method == "POST":
+        try:
+            query_db(
+                "INSERT INTO exercise (fk_section_id, txt_starting_code, txt_desired_output, ts_time_recorded) VALUES (?, ?, ?, ?);",
+                (
+                    request.json["section_id"],
+                    request.json["starting_code"],
+                    request.json["desired_output"],
+                    time.mktime(datetime.now().timetuple()),
+                ),
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
+
+        return list(dict(row) for row in query_db("SELECT * FROM exercise;"))
+
+    elif request.method == "GET":
+        try:
+            return list(
+                dict(row)
+                for row in query_db(
+                    "SELECT * FROM exercise WHERE fk_section_id = ? AND txt_starting_code LIKE ? AND txt_desired_output LIKE ? AND ts_time_recorded BETWEEN ? AND ?;",
+                    (
+                        request.json["section_id"],
+                        f'%{request.json["starting_code"]}%',
+                        f'%{request.json["desired_output"]}%',
+                        request.json["timestamp_min"],
+                        request.json["timestamp_max"],
+                    ),
+                )
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
 
 
 @app.route("/api/section", methods=["GET", "POST"])
-def add_section():
+def api_section():
+    """Handle GET and POST requests made to /api/section.
+    On a GET request, returns an array of dictionaries which represent the results of the query made
+    On a POST request, adds the designated section to the database and returns the entire section table to show that the record has been added.
+    """
     if request.method == "POST":
         try:
             query_db(
@@ -104,10 +177,10 @@ def add_section():
             return list(
                 dict(row)
                 for row in query_db(
-                    "SELECT * FROM section WHERE txt_section_name = ? AND txt_instructor_name = ? AND ts_time_recorded BETWEEN ? AND ?;",
+                    "SELECT * FROM section WHERE txt_section_name LIKE ? AND txt_instructor_name LIKE ? AND ts_time_recorded BETWEEN ? AND ?;",
                     (
-                        request.json["section_name"],
-                        request.json["instructor_name"],
+                        f'%{request.json["section_name"]}%',
+                        f'%{request.json["instructor_name"]}%',
                         request.json["timestamp_min"],
                         request.json["timestamp_max"],
                     ),
@@ -118,5 +191,63 @@ def add_section():
 
 
 @app.route("/api/submission}", methods=["GET", "POST"])
-def add_submission():
-    pass
+def api_submission():
+    """Handle GET and POST requests made to /api/section.
+    On a GET request, returns an array of dictionaries which represent the results of the query made
+    On a POST request, adds the designated section to the database and returns the entire section table to show that the record has been added.
+    """
+    if request.method == "POST":
+        try:
+            query_db(
+                "INSERT INTO student_submission (txt_student_program, txt_student_program_output, bool_is_complete, ts_starting_time, ts_submission_time, fk_exercise_id, fk_student_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+                (
+                    request.json["student_program"],
+                    request.json["student_program_output"],
+                    request.json["is_complete"],
+                    request.json["starting_time"],
+                    request.json["submission_time"],
+                    time.mktime(datetime.now().timetuple()),
+                    request.json["exercise_id"],
+                    request.json["student_id"],
+                ),
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
+
+        return list(dict(row) for row in query_db("SELECT * FROM student_submission;"))
+
+    elif request.method == "GET":
+        try:
+            return list(
+                dict(row)
+                for row in query_db(
+                    """
+                        SELECT * 
+                        FROM student_submission 
+                        WHERE 
+                            txt_student_program LIKE ? 
+                            AND txt_student_program_output LIKE ? 
+                            AND bool_is_complete = ? 
+                            AND ts_starting_time BETWEEN ? AND ?
+                            AND ts_submission_time BETWEEN ? AND ?
+                            AND ts_time_recorded BETWEEN ? AND ?
+                            AND fk_exercise_id = ?
+                            AND fk_student_id = ?;
+                    """,
+                    (
+                        f'%{request.json["student_program"]}%',
+                        f'%{request.json["student_program_output"]}%',
+                        request.json["is_complete"],
+                        request.json["starting_time_min"],
+                        request.json["starting_time_max"],
+                        request.json["submission_time_min"],
+                        request.json["submission_time_max"],
+                        request.json["time_recorded_min"],
+                        request.json["time_recorded_max"],
+                        request.json["exercise_id"],
+                        request.json["student_id"],
+                    ),
+                )
+            )
+        except Exception as e:
+            return f"An error occurred.\n{e}"
