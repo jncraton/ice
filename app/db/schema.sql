674a6c8a44828345ed760918df4f866255da11ae
jepoorbaugh <jepoorbaugh@gmail.com>
2024-11-13 10:46:54 -0500

    Implement API endpoints

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE section (
-    pk_section_id INTEGER PRIMARY KEY,
+    pk_section_id INTEGER PRIMARY KEY, -- alias for ROWID
     txt_section_name TEXT,
     txt_instructor_name TEXT,
     ts_time_recorded INTEGER
 ) STRICT;
 
 CREATE TABLE exercise (
-    pk_exercise INTEGER PRIMARY KEY,
-    fk_section INTEGER,
+    pk_exercise INTEGER PRIMARY KEY, -- alias for ROWID
+    fk_section_id INTEGER,
     txt_starting_code TEXT,
     txt_desired_output TEXT,
     ts_time_recorded INTEGER,
@@ -15,19 +15,21 @@ CREATE TABLE exercise (
 ) STRICT;
 
 CREATE TABLE student (
-    pk_student_id INTEGER PRIMARY KEY,
+    pk_student_id INTEGER PRIMARY KEY, -- alias for ROWID
     txt_student_name TEXT,
     fk_section_id INTEGER,
+    ts_time_recorded INTEGER,
     FOREIGN KEY (fk_section_id) REFERENCES section (pk_section_id)
 ) STRICT;
 
 CREATE TABLE student_submission (
-    pk_student_submission_id INTEGER PRIMARY KEY,
+    pk_student_submission_id INTEGER PRIMARY KEY, -- alias for ROWID
     txt_student_program TEXT,
     txt_student_program_output TEXT,
     bool_is_complete INTEGER,
     ts_starting_time INTEGER,
     ts_submission_time INTEGER,
+    ts_time_recorded INTEGER,
     fk_exercise_id INTEGER,
     fk_student_id INTEGER,
     FOREIGN KEY (fk_exercise_id) REFERENCES exercise (pk_exercise)
