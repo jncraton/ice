979604ed20edd6a56c0af768b0763b1fc439ebdd
dtlemon <124196309+dtlemon@users.noreply.github.com>
2024-11-13 10:05:53 -0500

    Delete duplicated lines (part 1)

--- tests/student_page_test.py
+++ tests/student_page_test.py
@@ -101,14 +101,6 @@ def test_check_output_incorrect(page: Page):
     targettext_locator = page.locator("#target-text")
     targettext_locator.evaluate("element => element.removeAttribute('disabled')")
     targettext_locator.fill("Hello World!")
-    # 1. Put code in code area
-    codearea_locator = page.locator("#code-area")
-    codearea_locator.fill("print('Hello Word')")
-
-    # 2. Put code in Desired output
-    targettext_locator = page.locator("#target-text")
-    targettext_locator.evaluate("element => element.removeAttribute('disabled')")
-    targettext_locator.fill("Hello World!")
 
     # 3. Click Run Button
     page.locator("#run-button").click()
