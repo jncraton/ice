0b369a0a75215ffadc4218216b4542f36b1077d0
JIckes12 <99823782+JIckes12@users.noreply.github.com>
2024-11-20 10:52:42 -0500

    Update page tests

--- tests/student_page_test.py
+++ tests/student_page_test.py
@@ -27,6 +27,10 @@ def test_python_runs(page: Page):
     Test that basic python code can execute
     """
 
+    #0. Insert name so page unlocks
+    page.locator("#student-name").fill("Student1")
+    page.locator("#start-button").click()
+
     # 1. Put code in code area
     page.locator("#student-name").fill("Student1")
     page.locator("#start-button").click()
@@ -46,6 +50,10 @@ def test_check_output_correct(page: Page):
     Test that the check output functionality works when the result of running the code
     matches the target output.
     """
+
+    #0. Insert name so page unlocks
+    page.locator("#student-name").fill("Student1")
+    page.locator("#start-button").click()
 
     # 1. Put code in code area
     page.locator("#student-name").fill("Student1")
@@ -73,6 +81,10 @@ def test_check_output_incorrect(page: Page):
     Test that the check output functionality works when the result of running the code
     does not match the target output.
     """
+
+    #0. Insert name so page unlocks
+    page.locator("#student-name").fill("Student1")
+    page.locator("#start-button").click()
 
     # 1. Put code in code area
     page.locator("#student-name").fill("Student1")
@@ -167,6 +179,10 @@ def test_infinite_loop_error_message(page: Page):
     """
     Test that an infinite loop is detected and feedback is given to the user.
     """
+    #0. Insert name so page unlocks
+    page.locator("#student-name").fill("Student1")
+    page.locator("#start-button").click()
+
     textarea_locator = page.locator("#code-area")
     textarea_locator.fill(
         """
